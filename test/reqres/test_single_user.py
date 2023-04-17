7a83078fe960207f8c50b0cb10dc872376a41f6e
a.skorik <a.skorik@petrovich.ru>
2023-04-17 17:47:45 +0300

    edited tests, worker.py

--- test/reqres/test_single_user.py
+++ test/reqres/test_single_user.py
@@ -1,7 +1,7 @@
 """Тест кейс для api reqres, single user"""
 import allure
 import pytest
-from helper.worker import load_tests
+from helper.worker import load_data
 
 pytest_plugins = ["fixture.reqres.reqres_api_fixture"]
 pytestmark = [allure.parent_suite("reqres"),
@@ -10,16 +10,16 @@ pytestmark = [allure.parent_suite("reqres"),
 
 @allure.title('Запрос с невалидным или отсутствующим параметром')
 @pytest.mark.parametrize(('user_id', 'expected_data'),
-                         load_tests('reqres.single_user_data', 'not_valid_data'))
-def test_single_user_wo_parameters(reqres_api, expected_data, user_id):
+                         load_data('reqres.single_user_data', 'not_valid_data'))
+def test_single_user_wo_parameters(reqres_api, user_id, expected_data):
     reqres_api.reqres_single_user(user_id).status_code_should_be(404).\
         have_value_in_response_parameter([], expected_data)
 
 
 @allure.title('Запрос c валидными параметрами')
 @pytest.mark.parametrize(('user_id', 'expected_data'),
-                         load_tests('reqres.single_user_data', 'data'))
-def test_single_user_valid_parameters(reqres_api, expected_data, user_id):
+                         load_data('reqres.single_user_data', 'data'))
+def test_single_user_valid_parameters(reqres_api, user_id, expected_data):
     reqres_api.reqres_single_user(user_id).status_code_should_be(200).\
         json_schema_should_be_valid('reqres.single_user_schema').\
         objects_should_be(expected_data, reqres_api.deserialize_register_payment())
